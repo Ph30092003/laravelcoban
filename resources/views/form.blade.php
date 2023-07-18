0dd4c7c260a396731fae686b97af68009b19e152
Phanz Namz <118036118+Ph30092003@users.noreply.github.com>
2023-07-18 11:06:31 +0700

    Update form.blade.php

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,4 +1,5 @@
 <form method="POST" action="/unicode">
+    <h1>Hello words</h1>
     <div>
         <input type="text" name="username" placeholder="Nhập họ và tên">
         <input type="hidden" name="_method" value="delete">
